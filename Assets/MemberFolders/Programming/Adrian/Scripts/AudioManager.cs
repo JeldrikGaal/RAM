fd9d71635f4dc718744a46a935b3b8a59eed8927
AdrianCV <sirmuffinhead@gmail.com>
2022-11-14 14:17:51 +0100

    Audio Manager
    Wowee audio manager draft

---
+++ Assets/MemberFolders/Programming/Adrian/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Public float value for volume of the audio clips
+    public float Volume;
+
+    /// Plays an audio clip at a certain position in the world with at a specific volume
+    // Input: Audio clip to be played, world position to play the audio clip at, volume for the clip
+    public void PlayAudio(AudioClip clip, Vector3 position)
+    {
+        AudioSource.PlayClipAtPoint(clip, position, Volume);
+        print("Playing audioclip: " + clip + " at: " + position);
+    }
+}
---
+++ Assets/MemberFolders/Programming/Adrian/Scripts/AudioTester.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTester : MonoBehaviour
+{
+    public AudioManager AudioManager;
+
+    [SerializeField] private AudioClip clipToPlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the test object 4 units in the x direction when you press W
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x + 4, transform.position.y, transform.position.z);
+        }
+
+        // Use the PlayAudio function from the AudioManager script to play an audioclip at the location of the gameObject
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AudioManager.PlayAudio(clipToPlay, transform.position);
+        }
+    }
+}
