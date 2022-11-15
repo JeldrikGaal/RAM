a681d5e385ec90142092238bc8180eccf6c7d919
Muratcan Bilgiç <murtcnnbilgic@gmail.com>
2022-11-15 11:37:40 +0100

    Health Bar UI is done and can be called from other scripts to update it

---
+++ Assets/MemberFolders/Programming/Muratcan/Scripts/HealthBar.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    Transform _cameraTransform;
+    GameObject _parentCharacter;
+    [SerializeField] Image _healthBarImage;
+    float _health = 1f;
+    [SerializeField] Color32 _playerHealthColor;
+    [SerializeField] Color32 _enemyHealthColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraTransform = Camera.main.transform;
+        _parentCharacter = transform.parent.gameObject;
+
+        if (_parentCharacter.CompareTag("Player"))
+        {
+            _healthBarImage.color = _playerHealthColor;
+        }
+        else
+        {
+            _healthBarImage.color = _enemyHealthColor;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        LookAtCamera();
+        UpdateHealthBar(0f);
+    }
+
+    void LookAtCamera()
+    {
+        transform.LookAt(_cameraTransform.position + transform.position);
+    }
+    /// <summary>
+    /// Use this to increase or decrease the health bar in events such as damage or health pick-up.
+    /// </summary>
+    /// <param name="changeAmount"></param>
+    public void UpdateHealthBar(float changeAmount)
+    {
+        _health += changeAmount;
+        _healthBarImage.fillAmount = _health;
+    }
+}
