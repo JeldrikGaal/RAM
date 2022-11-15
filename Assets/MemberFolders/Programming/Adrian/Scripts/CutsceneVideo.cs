4f867886d2beb0e8762231df4b50858380e4a113
AdrianCV <sirmuffinhead@gmail.com>
2022-11-15 13:36:27 +0100

    Cutscene functionality kinda

---
+++ Assets/MemberFolders/Programming/Adrian/Scripts/CutsceneVideo.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CutsceneVideo : MonoBehaviour
+{
+    private VideoPlayer _videoPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _videoPlayer.loopPointReached += EndReached;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _videoPlayer.frame += 40;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _videoPlayer.Play();
+        }
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        Destroy(gameObject);
+    }
+}
