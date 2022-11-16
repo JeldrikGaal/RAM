b3b027e3f5c351fb01eb7f9ed32c4a1fff5834b2
Norrun <trulvens@tutanota.com>
2022-11-16 15:26:27 +0100

    Starting work on pause menu

--- Assets/MemberFolders/Programming/Jeldrik/RammyInputActions.cs
+++ Assets/MemberFolders/Programming/Jeldrik/RammyInputActions.cs
@@ -345,6 +345,15 @@ public partial class @RammyInputActions : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Pause"",
+                    ""type"": ""Button"",
+                    ""id"": ""b75ef42d-de79-47cf-a14a-e3eb235c0f7c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -765,6 +774,17 @@ public partial class @RammyInputActions : IInputActionCollection2, IDisposable
                     ""action"": ""TrackedDeviceOrientation"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b8c5ceff-7972-467a-8e07-a08abbb91f6d"",
+                    ""path"": ""<Keyboard>/escape"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pause"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -851,6 +871,7 @@ public partial class @RammyInputActions : IInputActionCollection2, IDisposable
         m_UI_RightClick = m_UI.FindAction("RightClick", throwIfNotFound: true);
         m_UI_TrackedDevicePosition = m_UI.FindAction("TrackedDevicePosition", throwIfNotFound: true);
         m_UI_TrackedDeviceOrientation = m_UI.FindAction("TrackedDeviceOrientation", throwIfNotFound: true);
+        m_UI_Pause = m_UI.FindAction("Pause", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -985,6 +1006,7 @@ public partial class @RammyInputActions : IInputActionCollection2, IDisposable
     private readonly InputAction m_UI_RightClick;
     private readonly InputAction m_UI_TrackedDevicePosition;
     private readonly InputAction m_UI_TrackedDeviceOrientation;
+    private readonly InputAction m_UI_Pause;
     public struct UIActions
     {
         private @RammyInputActions m_Wrapper;
@@ -999,6 +1021,7 @@ public partial class @RammyInputActions : IInputActionCollection2, IDisposable
         public InputAction @RightClick => m_Wrapper.m_UI_RightClick;
         public InputAction @TrackedDevicePosition => m_Wrapper.m_UI_TrackedDevicePosition;
         public InputAction @TrackedDeviceOrientation => m_Wrapper.m_UI_TrackedDeviceOrientation;
+        public InputAction @Pause => m_Wrapper.m_UI_Pause;
         public InputActionMap Get() { return m_Wrapper.m_UI; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -1038,6 +1061,9 @@ public partial class @RammyInputActions : IInputActionCollection2, IDisposable
                 @TrackedDeviceOrientation.started -= m_Wrapper.m_UIActionsCallbackInterface.OnTrackedDeviceOrientation;
                 @TrackedDeviceOrientation.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnTrackedDeviceOrientation;
                 @TrackedDeviceOrientation.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnTrackedDeviceOrientation;
+                @Pause.started -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
+                @Pause.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
+                @Pause.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnPause;
             }
             m_Wrapper.m_UIActionsCallbackInterface = instance;
             if (instance != null)
@@ -1072,6 +1098,9 @@ public partial class @RammyInputActions : IInputActionCollection2, IDisposable
                 @TrackedDeviceOrientation.started += instance.OnTrackedDeviceOrientation;
                 @TrackedDeviceOrientation.performed += instance.OnTrackedDeviceOrientation;
                 @TrackedDeviceOrientation.canceled += instance.OnTrackedDeviceOrientation;
+                @Pause.started += instance.OnPause;
+                @Pause.performed += instance.OnPause;
+                @Pause.canceled += instance.OnPause;
             }
         }
     }
@@ -1141,5 +1170,6 @@ public partial class @RammyInputActions : IInputActionCollection2, IDisposable
         void OnRightClick(InputAction.CallbackContext context);
         void OnTrackedDevicePosition(InputAction.CallbackContext context);
         void OnTrackedDeviceOrientation(InputAction.CallbackContext context);
+        void OnPause(InputAction.CallbackContext context);
     }
 }
---
+++ Assets/MemberFolders/Programming/Trym/Scripts/PauseGame.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PauseGame : MonoBehaviour
+{
+    [SerializeField] GameObject _pauseMenu;
+    [SerializeField] UnityEvent _onPause;
+    [SerializeField] UnityEvent _onUnpause;
+    private static bool _paused;
+
+    RammyInputActions _inputs;
+    /// <summary>
+    /// Event for pausing the game, the argument is true when pausing.
+    /// </summary>
+    public static System.Action<bool> PausingEvent;
+    /// <summary>
+    /// toggles between paused and unpaused.
+    /// </summary>
+    private void Toggle()
+    {
+        if (_paused)
+        {
+            UnPause();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+    /// <summary>
+    /// Pauses the game.
+    /// </summary>
+    private void Pause()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0;
+        _pauseMenu.SetActive(true);
+        _paused = true;
+        PausingEvent(true);
+    }
+    /// <summary>
+    /// Unpauses the game
+    /// </summary>
+    public void UnPause()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+        _pauseMenu.SetActive(false);
+        _paused = false;
+        PausingEvent(false);
+    }
+
+    public static bool IsPaused() => _paused;
+    private void Start()
+    {
+        _inputs = new RammyInputActions();
+        _inputs.UI.Pause.Enable();
+        _inputs.UI.Pause.performed += (CallbackContext context) => Toggle();
+        
+    }
+}
