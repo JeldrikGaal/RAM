a892e2c5a7a24c834b4deb6167cdd731164fefa3
Norrun <trulvens@tutanota.com>
2022-11-17 11:47:35 +0100

    Cleaned up and finalized the preliminary pause menu
    Only thing not working is the settings button, main menu button should work when build index is properly sat up.

--- Assets/MemberFolders/Programming/Trym/Scripts/PauseGame.cs
+++ Assets/MemberFolders/Programming/Trym/Scripts/PauseGame.cs
@@ -13,16 +13,21 @@ public class PauseGame : MonoBehaviour
     [SerializeField] UnityEvent _onUnpause;
     private static bool _paused = false;
 
+    #region most of Input
     RammyInputActions _inputs;
-    /// <summary>
-    /// Event for pausing the game, the argument is true when pausing.
-    /// </summary>
-    public static System.Action<bool> PauseEvent;
-    #region Menu Interaction
+    private void Start()
+    {
+        _inputs = new RammyInputActions();
+        _inputs.UI.Pause.Enable();
+        _inputs.UI.Pause.performed += Toggle;
+
+    }
+    // Intened for pausing and unpausing with escape
+    
     /// <summary>
     /// toggles between paused and unpaused.
     /// </summary>
-    private void Toggle()
+    private void Toggle(CallbackContext context)
     {
         if (_paused)
         {
@@ -33,6 +38,13 @@ public class PauseGame : MonoBehaviour
             Pause();
         }
     }
+
+
+    #endregion
+
+
+    #region Menu Interaction
+
     /// <summary>
     /// Pauses the game.
     /// </summary>
@@ -61,10 +73,7 @@ public class PauseGame : MonoBehaviour
     /// <summary>
     /// restarting the level (Scene)
     /// </summary>
-    public void Restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+    public void Restart() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
     public void MainMenu() => SceneManager.LoadScene(0);
 
@@ -77,18 +86,21 @@ public class PauseGame : MonoBehaviour
     }*/
 
     #endregion
+
+    /// <summary>
+    /// Event for pausing the game, the argument is true when pausing.
+    /// </summary>
+    public static event System.Action<bool> PauseEvent;
+
+    
     /// <summary>
     /// To check if the game is paused.
     /// </summary>
     /// <returns></returns>
     public static bool IsPaused() => _paused;
-    private void Start()
-    {
-        _inputs = new RammyInputActions();
-        _inputs.UI.Pause.Enable();
-        _inputs.UI.Pause.performed += (CallbackContext context) => Toggle();
-        
-    }
+
+    #region private Misc
+
     //simple handler for the pause event
     private void OnPausedEventHandler(bool paused) => PauseEvent?.Invoke(paused);
 
@@ -106,8 +118,11 @@ public class PauseGame : MonoBehaviour
         if (_paused)
         {
             UnPause();
+            print("test");
         }
+        //removes old references
+        _inputs.UI.Pause.performed -= Toggle;
         
     }
-    
+    #endregion
 }
