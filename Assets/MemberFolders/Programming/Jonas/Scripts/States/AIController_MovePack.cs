65078fa8e9b6dcc084a6864822c69befd01e32cd
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2022-11-17 09:17:54 +0100

    Auto stash before merge of "main" and "origin/main"

--- Assets/MemberFolders/Programming/Jonas/Scripts/States/AIController_MovePack.cs
+++ Assets/MemberFolders/Programming/Jonas/Scripts/States/AIController_MovePack.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class AIController_MovePack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject _target;
+    private float _distance;
+    private float _rotationTime;
+
+    private Vector3 _point;
+
+    private AIController _packGroup;
+    private List<AI_MovePack> _packMembers;
+
+    public void Init(GameObject target, float distance, float rotationTime)
     {
-        
+        _target = target;
+        _packMembers = new List<AI_MovePack>();
+        _distance = distance;
+        _rotationTime = rotationTime;
+
+        _point = new Vector3(_distance, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _point = RotateAround(_point, Vector3.zero, new Vector3(0, 360 / _rotationTime * Time.deltaTime, 0));
+    }
+
+    public void AddMember(AI_MovePack member)
+    {
+        _packMembers.Add(member);
+    }
+
+    public void RemoveMember(AI_MovePack member)
+    {
+        _packMembers.Remove(member);
+
+        if (_packMembers.Count == 0)
+            Destroy(gameObject);
+    }
+
+    public Vector3 GetPoint(AI_MovePack member)
+    {
+        int memberPos = _packMembers.IndexOf(member);
+
+        float rotAngle = memberPos * (360 / _packMembers.Count);
+        Vector3 point = RotateAround(_point, Vector3.zero, new Vector3(0, rotAngle, 0));
+
+        return _target.transform.position + point;
+    }
+
+    private Vector3 RotateAround(Vector3 point, Vector3 pivot, Vector3 angle)
+    {
+        return Quaternion.Euler(angle) * (point - pivot) + pivot;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_target.transform.position + _point, .1f);
     }
 }
--- Assets/MemberFolders/Programming/Jonas/Scripts/States/AI_MovePack.cs
+++ Assets/MemberFolders/Programming/Jonas/Scripts/States/AI_MovePack.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class AI_MovePack : StateEffect
 {
     public float Weight;
+    public float Distance;
+    public float RotationTime;
     public AIController PackGroup;
+    public float Deadzone;
 
     private Jonas_TempCharacter _c;
     private AIController_MovePack _controller;
@@ -17,21 +20,32 @@ public class AI_MovePack : StateEffect
 
         _c = _user.GetComponent<Jonas_TempCharacter>();
 
+        // Connect to controller, create if none
         GameObject controllerGameObject = GameObject.Find($"AIController_MovePack{PackGroup.ToString()}");
         if (controllerGameObject == null)
         {
             controllerGameObject = new GameObject($"AIController_MovePack{PackGroup.ToString()}");
+            controllerGameObject.AddComponent<AIController_MovePack>();
+            controllerGameObject.GetComponent<AIController_MovePack>().Init(_target, Distance, RotationTime);
         }
+
+        _controller = controllerGameObject.GetComponent<AIController_MovePack>();
+        controllerGameObject.GetComponent<AIController_MovePack>().AddMember(this);
     }
 
     public override void OnEnd()
     {
-        // Remove self from controller, destroy controller if last
+        _controller.RemoveMember(this);
     }
 
     public override State OnUpdate()
     {
-        // Get
+        Vector3 followPos = (_controller.GetPoint(this));
+
+        if(Vector3.Distance(followPos, transform.position) > Deadzone)
+            _c.MoveInput += (followPos - _user.transform.position).normalized * Weight;
+        else
+            _c.MoveInput += (followPos - _user.transform.position).normalized * Weight/2;
 
         return null;
     }
