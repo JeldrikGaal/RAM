c95e9d62d86bd7cc6c44a212d139327b4bc306eb
James <haavardjames@outlook.com>
2022-11-17 11:48:45 +0100

    Added blood VFX to Rammy

--- Assets/MemberFolders/Programming/Havard/Blood splatter/Scripts/Effects/InitVelocity.cs
+++ Assets/MemberFolders/Programming/Havard/Blood splatter/Scripts/Effects/InitVelocity.cs
@@ -12,6 +12,11 @@ public class InitVelocity : MonoBehaviour
 
     // Settings for moving the objects away from the player
     [SerializeField] private bool awayFromPlayer = false;
+    public Vector3 CalcDirLeft;
+    public Vector3 CalcDirRight;
+    public float BloodForceMin;
+    public float BloodForceMax;
+
 
     // I anticipate 2 ways to get the directions depending on the way the player attacks:
     // If the player attacks in front of it, the direction should be the way the player is facing
@@ -20,9 +25,6 @@ public class InitVelocity : MonoBehaviour
 
     void Start()
     {
-        /// Temporary:
-        var player = GameObject.FindGameObjectsWithTag("Player");
-        /// End of temporary. Please delete.
 
         rb = GetComponent<Rigidbody>();
         rb.angularVelocity = new Vector3(Random.Range(angularVelocity.x, angularVelocity.y), Random.Range(angularVelocity.x, angularVelocity.y), Random.Range(angularVelocity.x, angularVelocity.y));
@@ -33,10 +35,8 @@ public class InitVelocity : MonoBehaviour
         } else if (awayFromPlayer == true)
         {
             // These two next lines needs to be swapped out with the two directions the blood should fall between
-            Vector3 calcDirLeft = player[0].transform.GetChild(2).transform.position - player[0].transform.GetChild(0).transform.position;
-            Vector3 calcDirRight = player[0].transform.GetChild(3).transform.position - player[0].transform.GetChild(0).transform.position;
 
-            ApplyVelocity(RandomVector3(calcDirLeft, calcDirRight), Random.Range(velocity.y - randomOffsetValue, velocity.y + randomOffsetValue));
+            ApplyVelocity(RandomVector3(CalcDirLeft, CalcDirRight), Random.Range(BloodForceMin, BloodForceMax));
         }
     }
     // Simple function that applies force with the direction. Can easily be modified.
