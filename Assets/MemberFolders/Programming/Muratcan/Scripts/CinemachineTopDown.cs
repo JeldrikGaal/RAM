082bcb4ad3b7cab261fecd1cc0bd0e5ac759a04b
Muratcan Bilgiç <murtcnnbilgic@gmail.com>
2022-11-17 15:53:26 +0100

    Work in progress Screen Shake System

--- Assets/MemberFolders/Programming/Muratcan/Scripts/CinemachineTopDown.cs
+++ Assets/MemberFolders/Programming/Muratcan/Scripts/CinemachineTopDown.cs
@@ -6,6 +6,9 @@ using Cinemachine;
 public class CinemachineTopDown : MonoBehaviour
 {
     CinemachineVirtualCamera _virtualCamera;
+    [SerializeField] AnimationCurve _curve;
+    [SerializeField] bool _shakeStart;
+    [SerializeField] float _shakeDuration = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,30 @@ public class CinemachineTopDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (_shakeStart)
+        {
+            StartCoroutine(CameraShake());
+            _shakeStart = false;
+        }
+    }
+    IEnumerator CameraShake()
+    {
         
+        Vector3 startPos = transform.position;
+        float _timePassed = 0f;
+
+        while (_timePassed < _shakeDuration)
+        {
+            _timePassed += Time.deltaTime;
+            float _strength = _curve.Evaluate(_timePassed / _shakeDuration);
+            transform.position = startPos + Random.insideUnitSphere * _strength;
+            yield return null;
+        }
+        transform.position = startPos;
+    }
+
+    public void ScreenShake()
+    {
+        _shakeStart = true;
     }
 }
