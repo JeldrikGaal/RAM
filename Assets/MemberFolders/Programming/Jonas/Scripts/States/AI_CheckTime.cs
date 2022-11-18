6a29cf2703bd925ac9913bdc2b898dd09f71ebce
Jonas Ingelstad <JonasIngelstad@Gmail.com>
2022-11-18 14:08:46 +0100

    Fixed AI timers

--- Assets/MemberFolders/Programming/Jonas/Scripts/States/AI_CheckTime.cs
+++ Assets/MemberFolders/Programming/Jonas/Scripts/States/AI_CheckTime.cs
@@ -8,29 +8,52 @@ public class AI_CheckTime : StateBlock
 
     private Dictionary<Jonas_TempCharacter, float> _timer;
 
-    public override void OnStart(Jonas_TempCharacter user, GameObject target)
-    {
-        if (_timer == null) _timer = new Dictionary<Jonas_TempCharacter, float>();
-
-        _timer[user] = TimeSec;
-    }
+    public override void OnStart(Jonas_TempCharacter user, GameObject target) { }
 
     public override (AI_State state, List<float> val) OnUpdate(Jonas_TempCharacter user, GameObject target)
     {
-        _timer[user] -= Time.deltaTime;
-
-        if (_timer[user] > 0)
-        {
-            List<float> temp = new List<float>();
-            temp.Add((float)StateReturn.Skip);
-            return (null, temp);
-        }
-
-        return (null, null);
+        List<float> temp = new List<float>();
+        temp.Add((float)StateReturn.Skip);
+        temp.Add(TimeSec);
+        return (null, temp);
     }
 
-    public override void OnEnd(Jonas_TempCharacter user, GameObject target)
-    {
-        _timer.Remove(user);
-    }
+    public override void OnEnd(Jonas_TempCharacter user, GameObject target) { }
 }
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class AI_CheckTime : StateBlock
+//{
+//    public float TimeSec;
+
+//    private Dictionary<Jonas_TempCharacter, float> _timer;
+
+//    public override void OnStart(Jonas_TempCharacter user, GameObject target)
+//    {
+//        if (_timer == null) _timer = new Dictionary<Jonas_TempCharacter, float>();
+
+//        _timer[user] = TimeSec;
+//    }
+
+//    public override (AI_State state, List<float> val) OnUpdate(Jonas_TempCharacter user, GameObject target)
+//    {
+//        _timer[user] -= Time.deltaTime;
+
+//        if (_timer[user] > 0)
+//        {
+//            List<float> temp = new List<float>();
+//            temp.Add((float)StateReturn.Skip);
+//            return (null, temp);
+//        }
+
+//        return (null, null);
+//    }
+
+//    public override void OnEnd(Jonas_TempCharacter user, GameObject target)
+//    {
+//        _timer.Remove(user);
+//    }
+//}
