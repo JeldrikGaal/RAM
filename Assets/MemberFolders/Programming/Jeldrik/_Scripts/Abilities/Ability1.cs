069771d34427c042ba48de5384a8455f4e0fa099
AdrianCV <sirmuffinhead@gmail.com>
2022-11-21 15:07:48 +0100

    Added Comments to Ability 1

--- Assets/MemberFolders/Programming/Jeldrik/_Scripts/Abilities/Ability1.cs
+++ Assets/MemberFolders/Programming/Jeldrik/_Scripts/Abilities/Ability1.cs
@@ -30,25 +30,37 @@ public class Ability1 : Abilities
     {
         base.Update();
 
+        // If the ability is activated
         if (_jumping)
         {
+            // Increased the jump timer
             _jumpTimer += Time.deltaTime;
+
+            // Sets the y position based on the animation curve
             transform.position = new Vector3(transform.position.x, _startPos.y + _yPosCurve.Evaluate(_jumpTimer), transform.position.z);
 
-
+            // Blocks the character from receiving movement inputs
             _controller.BlockPlayerMovment();
 
+            // If the timer has passed the last keyframe in the animation
             if (_jumpTimer > _yPosCurve.keys[_yPosCurve.keys.Length - 1].time)
             {
+                // Sets jumping to false
                 _jumping = false;
+
+                // Unblocks the movement
                 _controller.UnBlockPlayerMovement();
 
-                // transform.position = new Vector3(transform.position.x, _startPos.y, transform.position.z);
-
+                // Spawns the damage area at the players feet
                 var jumpArea = Instantiate(_damageArea, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), Quaternion.identity);
+
+                // Destroy the damage area after 0,5 seconds
                 Destroy(jumpArea, 0.5f);
+
+                // Checks if the ability is upgraded or not
                 if (Upgraded)
                 {
+                    // Starts the coroutine to spawn the upgraded damage area
                     StartCoroutine(SpawnUpgradedArea());
                 }
             }
@@ -56,8 +68,13 @@ public class Ability1 : Abilities
     }
     override public void Activate()
     {
+        // Activates the ability
         _jumping = true;
+
+        // Resets the jumping timer
         _jumpTimer = 0f;
+
+        // Saves the startpos of the jump
         _startPos = transform.position;
 
 
@@ -67,8 +84,13 @@ public class Ability1 : Abilities
 
     private IEnumerator SpawnUpgradedArea()
     {
+        // Waits for 0,5 seconds
         yield return new WaitForSeconds(0.5f);
+
+        // Spawns the upgraded damage area at the players feet
         var upgradedArea = Instantiate(_upgradedArea, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), Quaternion.identity);
-        Destroy(upgradedArea, 1f);
+
+        // Destroys the area after 0,5 seconds
+        Destroy(upgradedArea, 0.5f);
     }
 }
