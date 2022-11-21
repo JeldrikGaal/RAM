a66e08ef47802fc11cfe23e21772ac07e09ec114
Muratcan Bilgiç <murtcnnbilgic@gmail.com>
2022-11-21 10:15:05 +0100

    Screen shake effect is done. Can be called from other scripts wiht its method.

--- Assets/MemberFolders/Programming/Muratcan/Scripts/CinemachineTopDown.cs
+++ Assets/MemberFolders/Programming/Muratcan/Scripts/CinemachineTopDown.cs
@@ -6,12 +6,16 @@ using Cinemachine;
 public class CinemachineTopDown : MonoBehaviour
 {
     CinemachineVirtualCamera _virtualCamera;
-    [SerializeField] AnimationCurve _curve;
+    CinemachineImpulseSource _impulseSource;
+    [SerializeField] float screenShakeForce = 0.1f;
     [SerializeField] bool _shakeStart;
-    [SerializeField] float _shakeDuration = 1f;
     // Start is called before the first frame update
     void Start()
     {
+        //Necessary component for screen shake
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+
+        //Necessary component for camera follow
         _virtualCamera = gameObject.GetComponent<CinemachineVirtualCamera>();
         _virtualCamera.m_Follow = GameObject.FindGameObjectWithTag("Player").transform;
         transform.position = new Vector3(transform.position.x + 7.13f, transform.position.y + 18.03f, transform.position.z + -6.26f);
@@ -22,26 +26,14 @@ public class CinemachineTopDown : MonoBehaviour
     {
         if (_shakeStart)
         {
-            StartCoroutine(CameraShake());
+            _impulseSource.GenerateImpulseWithForce(screenShakeForce);
             _shakeStart = false;
         }
     }
-    IEnumerator CameraShake()
-    {
-        
-        Vector3 startPos = transform.position;
-        float _timePassed = 0f;
 
-        while (_timePassed < _shakeDuration)
-        {
-            _timePassed += Time.deltaTime;
-            float _strength = _curve.Evaluate(_timePassed / _shakeDuration);
-            transform.position = startPos + Random.insideUnitSphere * _strength;
-            yield return null;
-        }
-        transform.position = startPos;
-    }
-
+    /// <summary>
+    /// Use this to create a screen shake effect. Do not use this in continuous methods like Update(). Call this once every time you need it. Default force value is 0.1f. Can be tested in inspector with "Shake Start" bool.
+    /// </summary>
     public void ScreenShake()
     {
         _shakeStart = true;
