d1380866784b3f18aba485518c4a71a1161ba1db
James <haavardjames@outlook.com>
2022-11-22 10:57:06 +0100

    Updated RammyVFX
    Added some comments... not that I haven't been commenting...

--- Assets/MemberFolders/Programming/Havard/Blood splatter/Scripts/Effects/RammyVFX.cs
+++ Assets/MemberFolders/Programming/Havard/Blood splatter/Scripts/Effects/RammyVFX.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 
 public class RammyVFX : MonoBehaviour
 {
+    // Some essential things for the script:
     [SerializeField] private GameObject _bloodBomb;
     [SerializeField] private GameObject _bloodSpreadCalculator;
     [SerializeField] private BloodySteps _stepScript;
 
+    // These values are completely random, and helps variate the type of blood
+    [Header("Blood variations")]
+    [SerializeField] private GameObject[] _bloodVariations;
+
+    #region blood settings
+
+    // Here you can customize the values for every type of attack!
     [Header("Ram attack")]
     [Range(0.0f, 2.0f)] [SerializeField] private float _bloodSpread = 0.5f;
     [Range(0f, 90f)] public float _heightAngle = 20;
@@ -27,11 +35,12 @@ public class RammyVFX : MonoBehaviour
     [Range(0.1f, 2)] public float _bloodSizeMinNormal = 1;
     [Range(0.1f, 2)] public float _bloodSizeMaxNormal = 1;
 
-    [Header("Blood variations")]
-    [SerializeField] private GameObject[] _bloodVariations;
+    #endregion
 
 
+    #region blood functions
 
+    // These functions are called from the RammyController script, and invokes the main blood function with the values in this script, but with the enemy from the rammy script
     public void RamAttack(GameObject enemy)
     {
         SpawnBlood(_bloodSizeMin, _bloodSizeMax, _bloodSpread, _heightAngle, _bloodAmount, _bloodForceMin, _bloodForceMax, enemy);
@@ -41,32 +50,36 @@ public class RammyVFX : MonoBehaviour
         SpawnBlood(_bloodSizeMinNormal, _bloodSizeMaxNormal, _bloodSpreadNormal, _heightAngleNormal, _bloodAmountNormal, _bloodForceMinNormal, _bloodForceMaxNormal, enemy);
     }
 
+    #endregion
+
+    // This big function essentially sets up everything we need to make blood!
     private void SpawnBlood(float bloodSizeMin, float bloodSizeMax, float bloodSpread, float angle, float bloodAmount, float bloodForceMin, float bloodForceMax, GameObject rammedObject)
     {
+        // Here we instantiate the bloodprojectiles. The way I have it set up, lets it use one prefab with 15 projectiles inside it.
         var _bloodPrefab = Instantiate(_bloodBomb, rammedObject.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        
+        // Here we're inserting some settings to change and calculate the direction the blood should move in.
         _bloodPrefab.transform.localScale *= Random.Range(bloodSizeMin, bloodSizeMax);
         angle = -angle;
-
-        // Vector3 _enemyDirection = rammedObject.transform.position - this.transform.position;
         _bloodSpreadCalculator.transform.localRotation = Quaternion.Euler(angle, 0, 0);
-
         _bloodSpreadCalculator.transform.GetChild(0).transform.localScale = new Vector3(bloodSpread, 1, 1);
-
         var _bloodDir1 = _bloodSpreadCalculator.transform.GetChild(0).transform.GetChild(0).transform.position - _bloodSpreadCalculator.transform.position;
         var _bloodDir2 = _bloodSpreadCalculator.transform.GetChild(0).transform.GetChild(1).transform.position - _bloodSpreadCalculator.transform.position;
 
+        // This foreach lets us access all projectiles inside the prefab.
         var i = 0;
         foreach (Transform child in _bloodPrefab.transform)
         {
-
             int randomMaterialNum = Random.Range(0, _bloodVariations.Length);
 
-
+            // If we want less than 15, it deletes the rest! Probably not the best to spawn and delete the ones we don't need, but hey, that's bad programming for you.
             if (i >= bloodAmount)
             {
                 Destroy(child.gameObject);
             }
             i++;
+
+            // Here we're accessing the projectile scripts to change the final splat's settings and the projectile direction and force.
             child.GetComponent<StickyBlood>().BloodStepScript = _stepScript;
             child.GetComponent<StickyBlood>().BloodSize = Random.Range(bloodSizeMin, bloodSizeMax);
             child.GetComponent<StickyBlood>().SplatObject = _bloodVariations[randomMaterialNum];
