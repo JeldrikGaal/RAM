991986a6dfac8d79ebcd9362e61034bbc0d6486b
Muratcan Bilgiç <murtcnnbilgic@gmail.com>
2022-11-22 11:31:30 +0100

    In Progress 2nd Ability

--- Assets/MemberFolders/Programming/Jeldrik/_Scripts/Abilities/Ability2.cs
+++ Assets/MemberFolders/Programming/Jeldrik/_Scripts/Abilities/Ability2.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Ability2 : Abilities
 {
+    Rigidbody _rb;
+    Ray _ray;
+    [SerializeField] Collider[] hitColliders = new Collider[50];
+    [SerializeField] float range;
+    [SerializeField] float damage;
     public override void Start()
     {
         base.Start();
+        _rb = GetComponent<Rigidbody>();
     }
     override public void Update()
     {
@@ -14,6 +20,22 @@ public class Ability2 : Abilities
     }
     override public void Activate()
     {
-        Debug.Log("Ability 2");
+        //Creates a sphere and takes data's of everything in there
+        hitColliders = Physics.OverlapSphere(transform.position, range);
+
+        //To use until we get the animation
+        foreach (var item in hitColliders)
+        {
+            if (item.transform != null && item.transform.gameObject.GetComponent<EnemyTesting>())
+            {
+                item.transform.gameObject.GetComponent<EnemyTesting>().TakeDamage(damage, transform.up);
+            }
+        }
+    }
+    void OnDrawGizmos()
+    {
+        // Draws a yellow sphere at the transform's position
+        Gizmos.color = new Color(1, 1, 1, 0.3f);
+        Gizmos.DrawSphere(transform.position, range);
     }
 }
