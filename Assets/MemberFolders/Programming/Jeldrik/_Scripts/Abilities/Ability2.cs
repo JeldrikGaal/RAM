4d8c19bba753ff6d1e3936b95c9bfd16e5d08fd6
Muratcan Bilgiç <murtcnnbilgic@gmail.com>
2022-11-22 13:37:29 +0100

    2nd ability is done and ready for use.

--- Assets/MemberFolders/Programming/Jeldrik/_Scripts/Abilities/Ability2.cs
+++ Assets/MemberFolders/Programming/Jeldrik/_Scripts/Abilities/Ability2.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Ability2 : Abilities
 {
+    public int level = 0;
     Rigidbody _rb;
     Ray _ray;
     [SerializeField] Collider[] hitColliders = new Collider[50];
-    [SerializeField] float range = 5;
-    [SerializeField] float damage = 15;
+    [SerializeField] float range = 5f;
+    [SerializeField] float damage = 15f;
+    [SerializeField] float pushBackForce = 7f;
     public override void Start()
     {
         base.Start();
@@ -23,12 +25,14 @@ public class Ability2 : Abilities
         //Creates a sphere and takes data's of everything in there
         hitColliders = Physics.OverlapSphere(transform.position, range);
 
-        //To use until we get the animation
+        //Looks at everything physics catched and does the ability to those who has the enemy script.
         foreach (var item in hitColliders)
         {
             if (item.transform != null && item.transform.gameObject.GetComponent<EnemyTesting>())
             {
                 item.transform.gameObject.GetComponent<EnemyTesting>().TakeDamage(damage, transform.up);
+                item.transform.rotation = Quaternion.LookRotation(transform.position - item.transform.position);
+                item.transform.gameObject.GetComponent<Rigidbody>().AddForce(-item.transform.forward * pushBackForce, ForceMode.Impulse);
             }
         }
     }
