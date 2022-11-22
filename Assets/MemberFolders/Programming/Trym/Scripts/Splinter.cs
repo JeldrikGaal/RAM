582b4c4d78680a3c16e2709c3f3889df744cf5ee
Norrun <trulvens@tutanota.com>
2022-11-22 10:19:46 +0100

    Cleaning the code, and implementing knockback
    Knockback is is not tested

--- Assets/MemberFolders/Programming/Trym/Scripts/Splinter.cs
+++ Assets/MemberFolders/Programming/Trym/Scripts/Splinter.cs
@@ -8,18 +8,29 @@ using UnityEngine;
 
 public class Splinter : MonoBehaviour
 {
+    [SerializeField] Rigidbody _rigid;
+    Vector3 _initialVelocity;
 #if UNITY_EDITOR
     [SerializeField] bool _interactIrammableEditorOnly = false;
 #endif
+
+    private void OnEnable()
+    {
+        _initialVelocity = _rigid.velocity;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.HasTag("enemy"))
         {
+            // damaging the enemy
             collision.gameObject.GetComponent<EnemyTesting>().TakeDamage(0.1f, transform.up);
+            // adds knockback
+            collision.rigidbody.velocity =( _initialVelocity + _rigid.velocity ) /2;
         }
 
 #if UNITY_EDITOR
-        
+        // Editor only functionality for stresstesting.
         if (collision.gameObject.TryGetComponent(out IRammable rammable) && _interactIrammableEditorOnly )
         {
             if (rammable.Hit(gameObject))
