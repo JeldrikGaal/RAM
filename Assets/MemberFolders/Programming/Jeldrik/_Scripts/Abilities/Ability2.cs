e92fe4109f2f53e64e2bcd5b63bbd694c2c54611
Muratcan Bilgiç <murtcnnbilgic@gmail.com>
2022-11-23 09:47:32 +0100

    Added timing to ability 2

--- Assets/MemberFolders/Programming/Jeldrik/_Scripts/Abilities/Ability2.cs
+++ Assets/MemberFolders/Programming/Jeldrik/_Scripts/Abilities/Ability2.cs
@@ -7,10 +7,10 @@ public class Ability2 : Abilities
     public int level = 0;
     Rigidbody _rb;
     Ray _ray;
-    [SerializeField] Collider[] hitColliders = new Collider[50];
-    [SerializeField] float range = 5f;
-    [SerializeField] float damage = 15f;
-    [SerializeField] float pushBackForce = 7f;
+    [SerializeField] Collider[] _hitColliders = new Collider[50];
+    [SerializeField] float _range = 5f;
+    [SerializeField] float _damage = 15f;
+    [SerializeField] float _pushBackForce = 7f;
     public override void Start()
     {
         base.Start();
@@ -22,17 +22,22 @@ public class Ability2 : Abilities
     }
     override public void Activate()
     {
+        StartCoroutine(Attack());
+    }
+    IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(0.25f);
         //Creates a sphere and takes data's of everything in there
-        hitColliders = Physics.OverlapSphere(transform.position, range);
+        _hitColliders = Physics.OverlapSphere(transform.position, _range);
 
         //Looks at everything physics catched and does the ability to those who has the enemy script.
-        foreach (var item in hitColliders)
+        foreach (var item in _hitColliders)
         {
             if (item.transform != null && item.transform.gameObject.GetComponent<EnemyTesting>())
             {
-                item.transform.gameObject.GetComponent<EnemyTesting>().TakeDamage(damage, transform.up);
+                item.transform.gameObject.GetComponent<EnemyTesting>().TakeDamage(_damage, transform.up);
                 item.transform.rotation = Quaternion.LookRotation(transform.position - item.transform.position);
-                item.transform.gameObject.GetComponent<Rigidbody>().AddForce(-item.transform.forward * pushBackForce, ForceMode.Impulse);
+                item.transform.gameObject.GetComponent<Rigidbody>().AddForce(-item.transform.forward * _pushBackForce, ForceMode.Impulse);
             }
         }
     }
@@ -40,6 +45,6 @@ public class Ability2 : Abilities
     {
         // Draws a yellow sphere at the transform's position
         Gizmos.color = new Color(1, 1, 1, 0.3f);
-        Gizmos.DrawSphere(transform.position, range);
+        Gizmos.DrawSphere(transform.position, _range);
     }
 }
