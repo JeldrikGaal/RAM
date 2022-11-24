9f00f1b629357d7eeebc3190f6179f438ee9a4b1
James <haavardjames@outlook.com>
2022-11-24 15:36:09 +0100

    Cleaned up some code
    Nothing much

--- Assets/MemberFolders/Programming/Havard/Blood splatter/Scripts/Effects/RagdollVelocity.cs
+++ Assets/MemberFolders/Programming/Havard/Blood splatter/Scripts/Effects/RagdollVelocity.cs
@@ -9,13 +9,14 @@ public class RagdollVelocity : MonoBehaviour
     public float BloodForceMin;
     public float BloodForceMax;
 
-
     void Start()
     {
+        // Calculates a random direction and force, and multiplies them together:
         Vector3 randomDir = RandomVector3(CalcDirLeft, CalcDirRight);
         float randomForce = Random.Range(BloodForceMin, BloodForceMax);
         Vector3 finalVelocity = randomDir * randomForce;
-
+        
+        // Goes through every child, including childs of childs, checks if they are rigidbodies and gives them the velocity that we calculated.
         Transform[] allChildren = GetComponentsInChildren<Transform>();
         foreach (Transform child in allChildren)
         {
@@ -26,6 +27,7 @@ public class RagdollVelocity : MonoBehaviour
         }
     }
 
+    // Simple function that randomizes Vector3's.
     public static Vector3 RandomVector3(Vector3 min, Vector3 max)
     {
         return new Vector3(Random.Range(min.x, max.x), Random.Range(min.y, max.y), Random.Range(min.z, max.z));
