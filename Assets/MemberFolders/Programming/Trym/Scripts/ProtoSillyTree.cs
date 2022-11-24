a0f56736827f92f5cca910db22f9ea529c4019f1
Norrun <trulvens@tutanota.com>
2022-11-24 13:59:44 +0100

    Silly tree prototype code hopefully done

--- Assets/MemberFolders/Programming/Trym/Scripts/ProtoSillyTree.cs
+++ Assets/MemberFolders/Programming/Trym/Scripts/ProtoSillyTree.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class ProtoSillyTree : MonoBehaviour, IRammable
 {
+    private Quaternion _modifiedRotation;
+    private Quaternion _originalRotation;
+    private Quaternion _targetRotation;
+    private Vector3 _originalPosition;
+    [SerializeField] private Rigidbody _rigid;
+    [SerializeField] private float _fallTime;
+    private float _hitTime = -999;
+    [SerializeField] private float _height;
+    [SerializeField] AnimationCurve curve;
+    
     public bool Hit(GameObject g)
     {
         var fromPlayer = Quaternion.LookRotation(-(g.transform.position - transform.position));
 
+        _originalRotation = transform.rotation;
+        _modifiedRotation = Quaternion.Euler(fromPlayer.eulerAngles - transform.rotation.eulerAngles);
 
+        _hitTime = Time.time;
+        
         return true;
     }
 
@@ -19,9 +33,16 @@ public class ProtoSillyTree : MonoBehaviour, IRammable
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        if ((Time.time - _hitTime) / _fallTime <= 1)
+        {
+            var rotation = Quaternion.Lerp(_originalRotation, _targetRotation, curve.Evaluate((Time.time - _hitTime) / _fallTime));
+            _rigid.Move(PositionAroundThePoint(rotation.eulerAngles,_height/2),rotation);
+
+            
+        }
         
     }
+    private Vector3 PositionAroundThePoint(Vector3 eulerAngles, float radius) => (_originalPosition - Vector3.up * (_height / 2)) + (Quaternion.Euler(_originalRotation.eulerAngles + eulerAngles) * (Vector3.up * radius));
 }
