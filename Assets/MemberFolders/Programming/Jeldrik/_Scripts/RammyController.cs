b09f8e818032ee35de1b036d0e81874837f204c1
AdrianCV <sirmuffinhead@gmail.com>
2022-11-25 12:06:10 +0100

    Object fall from tree kinda

--- Assets/MemberFolders/Programming/Jeldrik/_Scripts/RammyController.cs
+++ Assets/MemberFolders/Programming/Jeldrik/_Scripts/RammyController.cs
@@ -852,7 +852,10 @@ public class RammyController : MonoBehaviour
             // Makes the bridge rotate when it is rammed
             rammedObject.GetComponent<KnockDownBridgeScript>().CanRotate = true;
         }
-
+        else if (TagManager.HasTag(rammedObject, "objectfalltree"))
+        {
+            rammedObject.GetComponent<ObjectFallFromTree>().DropItem = true;
+        }
     }
 
     // Checking for any collisions Rammy encouters and reacting accordingly
