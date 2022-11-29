436f29eb334b91dd57cdbfc93a34c6c7c7ed1fad
Norrun <trulvens@tutanota.com>
2022-11-29 14:51:57 +0100

    Working on the bears bombs

---
+++ Assets/MemberFolders/Programming/Trym/Scripts/GenericBearBomb.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericBearBomb : MonoBehaviour
+{
+    [SerializeField] GameObject _effect;
+    [SerializeField] float _fuse;
+    [SerializeField] Vector3 _effectPosMod;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.attachedRigidbody != null)
+        {
+            if (collision.collider.attachedRigidbody.isKinematic)
+            {
+                StartCoroutine(Counting());
+            }
+        }
+        else
+        {
+            StartCoroutine(Counting());
+        }
+        
+    }
+
+    //activate when the fuse is finished.
+    IEnumerator Counting()
+    {
+        yield return new WaitForSeconds(_fuse);
+        _effect.SetActive(true);
+        _effect.transform.LookAt(transform.position + Vector3.forward, Vector3.up);
+        _effect.transform.parent = null;
+        _effect.transform.position = transform.position + _effectPosMod;
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Overrides the properties of the bomb
+    /// </summary>
+    /// <param name="effect"></param>
+    /// <param name="fuseTime"></param>
+    public void SetProperties(GameObject effect,float fuseTime)
+    {
+        _effect = effect;
+        _fuse = fuseTime;
+    }
+
+}
