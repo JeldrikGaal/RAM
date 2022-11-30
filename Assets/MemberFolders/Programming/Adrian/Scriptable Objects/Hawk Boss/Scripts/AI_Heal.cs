07ae374bca59c2f4a172d10540b41f5493467295
AdrianCV <sirmuffinhead@gmail.com>
2022-11-30 12:21:34 +0100

    Boss stuff

---
+++ Assets/MemberFolders/Programming/Adrian/Scriptable Objects/Hawk Boss/Scripts/AI_Heal.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Heal : StateBlock
+{
+    [SerializeField] private bool _fullHeal;
+    [SerializeField] private float _healAmount;
+    public override void OnStart(EnemyController user, GameObject target)
+    {
+
+    }
+
+    public override (AI_State state, List<float> val) OnUpdate(EnemyController user, GameObject target)
+    {
+        if (_fullHeal)
+        {
+            user.Health = user.MaxHealth;
+        }
+        else
+        {
+            user.Health += _healAmount;
+        }
+        return (null, null);
+    }
+
+    public override void OnEnd(EnemyController user, GameObject target)
+    {
+
+    }
+}
