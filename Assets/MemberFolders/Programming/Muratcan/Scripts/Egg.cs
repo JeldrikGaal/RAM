bbfbffa0f72afd4c2d5767ab631b2ec40d64549d
Muratcan Bilgiç <murtcnnbilgic@gmail.com>
2022-11-30 10:31:50 +0100

    Update on rammy script

---
+++ Assets/MemberFolders/Programming/Muratcan/Scripts/Egg.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : MonoBehaviour
+{
+    public float damage = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<RammyController>())
+        {
+            collision.gameObject.GetComponent<RammyController>().TakeDamageRammy(damage);
+        }
+    }
+}
