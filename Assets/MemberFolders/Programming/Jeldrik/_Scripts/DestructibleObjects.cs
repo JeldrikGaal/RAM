30d0e3640717cea56a9b01db04dbb2aef6240ed9
Jeldrik Gaal <jeldrikgaal@gmail.com>
2022-12-12 11:45:50 +0100

    Starting work on making boxxes and stuff breakable

--- Assets/MemberFolders/Programming/Jeldrik/_Scripts/DestructibleObjects.cs
+++ Assets/MemberFolders/Programming/Jeldrik/_Scripts/DestructibleObjects.cs
@@ -2,24 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestructibleWall : MonoBehaviour, IRammable
+public class DestructibleObjects : MonoBehaviour, IRammable
 {
-
-    public GameObject Complete;
-    public GameObject Broken;
     private BoxCollider _collider;
     [SerializeField] private GameObject _destroyParticle;
 
     // Start is called before the first frame update
     void Start()
     {
-        _collider = GetComponent<BoxCollider>();    
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _collider = GetComponent<BoxCollider>();
     }
 
     public bool Hit(GameObject g)
@@ -29,8 +20,8 @@ public class DestructibleWall : MonoBehaviour, IRammable
             Instantiate(_destroyParticle, transform.position, transform.rotation);
         }
         _collider.enabled = false;
-        Broken.SetActive(true);
-        Complete.SetActive(false);
         return false;
     }
+
+
 }
