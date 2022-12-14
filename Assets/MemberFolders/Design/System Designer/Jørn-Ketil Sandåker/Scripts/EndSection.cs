a98b53486b8334039b0b022734716f2a03077251
Jørn Ketil Sandåker <jornketilsandaker@gmail.com>
2022-12-14 13:51:15 +0100

    EndSection prefab and script

---
+++ Assets/MemberFolders/Design/System Designer/Jørn-Ketil Sandåker/Scripts/EndSection.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EndSection : MonoBehaviour
+{
+    public GameObject enemies;
+    public WinCondition _winCondition;
+
+    [SerializeField] private GameObject sectionWall;
+
+    public enum WinCondition // What type of condition to be met to end the section
+    {
+        Enemies,
+        Letters,
+        Elites
+    };
+
+    public bool done;
+
+    void Update()
+    {
+        //Debug.Log(_winCondition);
+        if (_winCondition == WinCondition.Enemies || _winCondition == WinCondition.Elites)
+        {
+            if (enemies.transform.childCount == 0)
+            {
+                done = true;
+                sectionWall.SetActive(false);
+            }
+        }
+        else if (_winCondition == WinCondition.Letters)
+        {
+            //Lettercode
+        }
+    }
+
+	private void OnTriggerExit(Collider other)
+	{
+        if (other.gameObject.HasTag("player") && done)
+        {
+            sectionWall.SetActive(true);
+        }
+    }
+}
