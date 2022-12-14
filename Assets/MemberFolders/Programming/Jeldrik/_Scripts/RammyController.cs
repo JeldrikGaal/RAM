a5840ece6191bf8da3b6f2b27cd6b0d267f6bcd5
James <haavardjames@outlook.com>
2022-12-14 13:47:08 +0100

    Fixed it so Rammy can ram through gore for real this time

--- Assets/MemberFolders/Programming/Jeldrik/_Scripts/RammyController.cs
+++ Assets/MemberFolders/Programming/Jeldrik/_Scripts/RammyController.cs
@@ -915,7 +915,7 @@ public class RammyController : MonoBehaviour
             //int layer = 1 << LayerMask.NameToLayer("Default");
             int layer = 1 << 11;
             layer |= (1 << 20);
-            layer |= (1 << 18);
+            layer |= (1 << 6);
             layer = ~layer;
             // int layer = 20;
 
